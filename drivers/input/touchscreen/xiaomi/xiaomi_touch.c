TPP-Devices/android_kernel_xiaomi_sm8450

input: touchscreen: xiaomi: Avoid NPE if getModeValue is not registered
Change-Id: Ia7a70e1823aa36039634bafa8dcd8994139bed06
Signed-off-by: Jens Reidel <adrian@travitia.xyz>

drivers/input/touchscreen/xiaomi/xiaomi_touch.c
@@ -1692,7 +1692,7 @@ static ssize_t gesture_single_tap_enabled_store(struct device *dev,
 	}
 	touch_data = touch_pdata->touch_data[0];
 
-	if (!touch_data->setModeValue)
+	if (!touch_data->setModeValue || !touch_data->getModeValue)
 		return -ENOMEM;
 
 	if (sscanf(buf, "%d", &input) < 0 || input > 1)
@@ -1758,7 +1758,7 @@ static ssize_t gesture_double_tap_enabled_store(struct device *dev,
 	}
 	touch_data = touch_pdata->touch_data[0];
 
-	if (!touch_data->setModeValue)
+	if (!touch_data->setModeValue || !touch_data->getModeValue)
 		return -ENOMEM;
 
 	if (sscanf(buf, "%d", &input) < 0 || input > 1)
@@ -1816,7 +1816,7 @@ fod_longpress_gesture_enabled_store(struct device *dev,
 	}
 	touch_data = touch_pdata->touch_data[0];
 
-	if (!touch_data->setModeValue)
+	if (!touch_data->setModeValue || !touch_data->getModeValue)
 		return -ENOMEM;
 
 	if (sscanf(buf, "%d", &input) < 0 || input > 1)

ishtar-aosp/kernel_xiaomi_sm8550

drivers/input/touchscreen/xiaomi/xiaomi_touch.c
@@ -1649,7 +1649,7 @@ static ssize_t gesture_single_tap_enabled_store(struct device *dev,
 	}
 	touch_data = touch_pdata->touch_data[0];
 
-	if (!touch_data->setModeValue)
+	if (!touch_data->getModeValue || !touch_data->setModeValue)
 		return -ENOMEM;
 
 	if (sscanf(buf, "%d", &input) < 0 || input > 1)
@@ -1715,7 +1715,7 @@ static ssize_t gesture_double_tap_enabled_store(struct device *dev,
 	}
 	touch_data = touch_pdata->touch_data[0];
 
-	if (!touch_data->setModeValue)
+	if (!touch_data->getModeValue || !touch_data->setModeValue)
 		return -ENOMEM;
 
 	if (sscanf(buf, "%d", &input) < 0 || input > 1)
@@ -1773,7 +1773,7 @@ fod_longpress_gesture_enabled_store(struct device *dev,
 	}
 	touch_data = touch_pdata->touch_data[0];
 
-	if (!touch_data->setModeValue)
+	if (!touch_data->getModeValue || !touch_data->setModeValue)
 		return -ENOMEM;
 
 	if (sscanf(buf, "%d", &input) < 0 || input > 1)